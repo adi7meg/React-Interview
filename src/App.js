6eb4f9ba5d400ee2294d93c61aca602918b27a6e
Aditya Sharma <aditya.sharma@jarvis.consulting>
2023-08-23 20:17:56 +0530

    first commit~

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+  const [search, setSearch] = useState('');
+  const [posts, setPosts] = useState([]); // Store all posts from API
+  const [filteredPosts, setFilteredPosts] = useState([]); // Store filtered posts
+
+  const getApi = () => {
+    axios.get('https://jsonplaceholder.typicode.com/posts')
+      .then((res) => {
+        setPosts(res.data);
+      })
+      .catch((error) => {
+        console.error('Error fetching data:', error);
+      });
+  };
+
+  useEffect(() => {
+    getApi();
+  }, []);
+
+  const handleSearch = () => {
+    // Filter posts based on the search term
+    const filtered = posts.filter(post =>
+      post.title.toLowerCase().includes(search.toLowerCase())
+    );
+    setFilteredPosts(filtered);
+  };
+
+  const handleChange = e => {
+    setSearch(e.target.value);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="search-container">
+        <input className="search-input" onChange={handleChange} value={search} placeholder="Enter search text" />
+        <button className="search-button" onClick={handleSearch}>Search</button>
+      </div>
+      <div className="posts-container">
+        {(search !== '' ? filteredPosts : posts).map(post => (
+          <div className="post" key={post.id}>{post.title}</div>
+        ))}
+      </div>
     </div>
   );
 }
